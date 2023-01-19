f53e2408e3d8298c46af45ebb06c42298dc6e48d
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-20 02:27:49 +0500

    getter and setters

--- build/js/classes.js
+++ build/js/classes.js
@@ -9,10 +9,19 @@ class Person {
    greet () {
       return `Hello ${this.name}`
    }
+   get info () {  // getter
+      console.log( `Name:${this.name} with age ${this.age}` );
+   }
+   set money ( money ) {
+      if ( money < 0 ) return
+      this.#money = money
+   }
 
 }
 
 const jimmy = new Person( 'jimmy', 12 )
 
 jimmy.greet()
+jimmy.info
+jimmy.money = -1
 console.log( jimmy )
